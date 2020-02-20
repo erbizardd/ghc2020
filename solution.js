84c6cac3fd6e6a2c8eb5ec72302fae5dc76abd2a
David Vandergucht <david.vandergucht@accenture.com>
2020-02-20 19:38:47 +0100

    started lib score

--- solution.js
+++ solution.js
@@ -1,5 +1,25 @@
+
 exports.process = (data) => {
+
+
+
+    function computeLibScores(valuesBook, lib) {
+
+
+        scores = {}
+
+        scoreMap = {}
+        lib.listBook.forEach(bookIndex => {
+            scoreMap[valuesBook[bookIndex]] = bookIndex;                
+        });
+        lib.scoreMap = scoreMap;
+    }
+    
+
     return new Promise((resolve) => {
     	resolve(data.array)
     })
 }
+
+valueBook -> scores
+libs: listBook index
