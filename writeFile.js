fc8a529f825e2a818a55b4a0b731951e8621d7a2
Yohann Perraud <yohann-externe.perraud@enedis.fr>
2020-02-20 21:18:34 +0100

    Ecriture du fichier de resp

--- writeFile.js
+++ writeFile.js
@@ -1,4 +1,30 @@
 const fs = require("fs");
+
+/**
+ * Prend en param une resp de la forme : 
+ * let res = 
+ * [
+        {
+            "indexLib": 1,
+            "scannedBooks": [5, 2, 3]
+
+        },
+        {
+            "indexLib": 0,
+            "scannedBooks": [0, 1, 2, 3, 4]
+        }
+    ];
+ */
 exports.writeFile =(resp, fileName) =>  {
-    fs.writeFileSync(`results/${fileName}.resp`, `${resp.length}\n${JSON.stringify(resp.reverse()).slice(1, -1).replace(/,/g, " ")}`, 'utf8');
+
+    let output = `${resp.length}\n`;
+
+    resp.forEach(lib => {
+        output += `${lib.indexLib} ${lib.scannedBooks.length}\n`;
+        output += `${JSON.stringify(lib.scannedBooks).slice(1, -1).replace(/,/g, " ")}\n`;
+    });
+
+    console.log(output);
+
+    fs.writeFileSync(`results/${fileName}.resp`, output, 'utf8');
 }
