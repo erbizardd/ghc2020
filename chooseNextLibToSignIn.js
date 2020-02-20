499ba34880a6df274087ac8386742cfd87fd76d7
David Vandergucht <david.vandergucht@accenture.com>
2020-02-20 21:10:03 +0100

    package json wsl et dos

--- chooseNextLibToSignIn.js
+++ chooseNextLibToSignIn.js
@@ -1,5 +1,5 @@
-module.exports((data)=>{
+module.exports = (data)=>{
     //signeUpLibs c'est une hashmap, pas un array
     console.log(data)
     return data.libs.filter((lib) => !lib.busy).pop()   
-});
+};
