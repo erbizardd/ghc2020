afe4f2e0f7f4252e280f45129f97fe845a1c22a2
David Vandergucht <david.vandergucht@accenture.com>
2020-02-20 20:31:30 +0100

    debut moteur A

---
+++ chooseNextLibToSignIn.js
@@ -0,0 +1,5 @@
+module.exports((data, signedUpLibs)=>{
+    //signeUpLibs c'est une hashmap, pas un array
+    var nextLib = null;
+    return nextLib;
+});
---
+++ engine1.js
@@ -0,0 +1,13 @@
+chooseNextLibToSignIn = require("./chooseNextLibToSignIn");
+module.exports(data=>{
+
+    var scannedBook = {}
+    var signedUpLibs = {}
+
+    for (var i = 0; i<= data.tpstt; i++) {
+
+    }
+
+
+
+});
