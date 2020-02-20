8118f2208329dd390921cb7ecad2d5b0e6b7ccfc
David Vandergucht <david.vandergucht@accenture.com>
2020-02-20 19:56:42 +0100

    sort idx by score

--- solution.js
+++ solution.js
@@ -1,25 +1,25 @@
 
 exports.process = (data) => {
 
-
+    console.log(data);
 
     function computeLibScores(valuesBook, lib) {
 
 
-        scores = {}
+        var scores = {}
 
-        scoreMap = {}
+        var scoreMap = {}
         lib.listBook.forEach(bookIndex => {
             scoreMap[valuesBook[bookIndex]] = bookIndex;                
         });
+        var idxOrdererd = []
+        Object.keys(scoreMap).reverse().forEach((k)=>idxOrdererd.push(scoreMap[k]));
         lib.scoreMap = scoreMap;
+        lib.idxByScoreRev = idxOrdererd;
     }
-    
 
+    data.libs.forEach((lib)=>computeLibScores(data.valueBook, lib));
     return new Promise((resolve) => {
     	resolve(data.array)
     })
 }
-
-valueBook -> scores
-libs: listBook index
